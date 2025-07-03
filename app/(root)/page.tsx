887489e5606645e653b42fc4fa5139a7ac3e85b1
Luke Canvin <lukecanvin@gmail.com>
2025-07-03 10:41:22 +0100

    Search form

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,13 @@
 import Image from "next/image";
+import SearchForm from "@/components/SearchForm";
 
-export default function Home() {
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) {
+  const query = (await searchParams).query;
+
   return (
     <>
       <section className="pink_container">
@@ -12,6 +19,7 @@ export default function Home() {
         <p className="subheading !max-w-3xl">
           Submit ideas, vote on pitches and get noticed in virtual competitions.
         </p>
+        <SearchForm query={query} />
       </section>
     </>
   );
